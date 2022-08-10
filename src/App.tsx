7ad53da6d4a96490f53672c547e9920c5859aca6
Aleksandr Shtykov <aleksandr.shtykov@prospa.com>
2022-08-10 15:37:53 +1000

    example to replicate

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Card } from 'reactstrap'
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Card>Test</Card>
       </header>
     </div>
   );
